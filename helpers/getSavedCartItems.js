7839819536cc60baf034479670fe58e38e9bae9f
Erik Yamamoto <erik.ymmt@gmail.com>
2022-06-06 18:56:10 -0300

    fix tests for getSavedItem

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,6 +1,8 @@
 const getSavedCartItems = () => {
   const savedCart = localStorage.getItem('cartItems');
-  document.querySelector('.cart__items').innerHTML = savedCart;
+  if (savedCart !== undefined) {
+    document.querySelector('.cart__items').innerHTML = savedCart;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -7,6 +7,6 @@ describe('4 - Teste a função getSavedCartItems', () => {
   it('should call localStorage.getItem when <ol><li>Item</li></ol> is the argumenet', async () => {
     await getSavedCartItems();
     expect(localStorage.getItem).toBeCalled();
-    expect(localStorage.getItem).toBeCalledWith(cartItems);
+    expect(localStorage.getItem).toBeCalledWith('cartItems');
   });
 });
