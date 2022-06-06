4b1d57a65f59e3a338bd760150eb26cdb1b6c636
Erik Yamamoto <erik.ymmt@gmail.com>
2022-06-05 21:53:34 -0300

    add requirement 9 - calculate total price

--- script.js
+++ script.js
@@ -33,10 +33,33 @@ const insertAvailableProducts = async () => {
 
 const getIDFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
+// Opção 1 - quando for mudado o estilo do carrinho
+// const calculateTotalPrice = () => {
+//   const totalPriceTag = document.querySelector('.total-price');
+//   const allPrices = document.querySelectorAll('.product-price');
+//   allPricesArray = Array.from(allPrices);
+//   const totalPrice = allPricesArray.reduce((total, item) =>
+//     total + parseInt(item.innerHTML, 10), 0);
+//   console.log(totalPrice);
+//   totalPriceTag.innerText = totalPrice;
+// };
+
+const calculateTotalPrice = () => {
+  const totalPriceTag = document.querySelector('.total-price');
+  const cart = document.querySelectorAll('.cart__item');
+  const cartArray = Array.from(cart);
+  const totalPrice = cartArray.reduce((total, currentProduct) => {
+    const currentPrice = currentProduct.innerText.split('$')[1];
+    return total + parseFloat(currentPrice);
+  }, 0);
+  totalPriceTag.innerHTML = Math.round(totalPrice * 100) / 100;
+};
+
 const cartItemClickListenerDelete = (event) => {
   const cart = document.querySelector('.cart__items');
   cart.removeChild(event.target);
   saveCartItems(cart.innerHTML);
+  calculateTotalPrice();
 };
 
 const cartItemClickListener = () => {
@@ -50,6 +73,9 @@ const createCartItemElement = ({ id, title, price }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
+  // li.innerHTML = `ID: ${id}
+  // <br>NOME: ${title}
+  // <br>PREÇO: $<span class="product-price">${price}</span>`;
   li.addEventListener('click', cartItemClickListenerDelete);
   return li;
 };
@@ -66,6 +92,7 @@ const addProductOnCart = async () => {
       const cart = document.querySelector('.cart__items');
       cart.appendChild(newItemOnCart);
       saveCartItems(cart.innerHTML);
+      calculateTotalPrice();
     });
   });
 };
@@ -74,4 +101,5 @@ window.onload = () => {
   addProductOnCart();
   getSavedCartItems();
   cartItemClickListener();
+  calculateTotalPrice();
 };
